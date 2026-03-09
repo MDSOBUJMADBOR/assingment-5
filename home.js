3d4dffc4a9900b38f02ac802701acc2b6ff35dd0
MDSOBUJMADBOR <sobujmadbor660@gmail.com>
2026-03-09 21:14:27 +0600

    assingment-5 completed

--- home.js
+++ home.js
@@ -72,21 +72,6 @@ hideLoading()
 
 loadAllCard();
 
-// "id": 1,
-// "title": "Fix navigation menu on mobile devices",
-// "description": "The navigation menu doesn't collapse properly on mobile devices. Need to fix the responsive behavior.",
-// "status": "open",
-// "labels": [
-// "bug",
-// "help wanted"
-// ],
-// "priority": "high",
-// "author": "john_doe",
-// "assignee": "jane_smith",
-// "createdAt": "2024-01-15T10:30:00Z",
-// "updatedAt": "2024-01-15T10:30:00Z"
-
-
 
 // Display cards
 function displayIssues(issues) {
@@ -94,16 +79,7 @@ function displayIssues(issues) {
   treesContainer.innerHTML = "";
 
   issues.forEach(issue => {
-
-// const label = issue.labels
-// label.forEach(lab => {
-//   console.log(lab);
-// })
   
-
-
-
-    // console.log(issue);
     const card = document.createElement("div");
 
     card.className = `max-w-[380px] border-t-4 ${
@@ -224,8 +200,6 @@ document.getElementById("btn-search").addEventListener("click", () => {
 function calculateCount() {
 allCount.innerText = treesContainer.children.length;
 }
-
-//-------------------------
 
 
 
